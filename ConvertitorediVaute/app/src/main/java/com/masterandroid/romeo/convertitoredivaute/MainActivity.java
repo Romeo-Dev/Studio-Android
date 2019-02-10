168bb410e7fab1d849e1fedd0030beeabfa7aedd
Romeo <romeodev1996@gmail.com>
2019-02-10 20:36:33 +0100

    Applicazione android ultimata, creato debug per importo nullo grazie al metodo isEmpty(), creato toast per info utente formato stringa migliorato tramite String.format()

--- ConvertitorediVaute/app/src/main/java/com/masterandroid/romeo/convertitoredivaute/MainActivity.java
+++ ConvertitorediVaute/app/src/main/java/com/masterandroid/romeo/convertitoredivaute/MainActivity.java
@@ -2,14 +2,46 @@ package com.masterandroid.romeo.convertitoredivaute;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.masterandroid.romeo.convertitoredivaute.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText Euro;
+    TextView resDollari;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Euro = (EditText) findViewById(R.id.importo);
+        resDollari = (TextView) findViewById(R.id.resDollari);
+
+    }
+
+
+    public void converti(View view) {
+        final double tasso= 0.72;
+        // prendo il valore dell importo
+
+        if (!isEmpty(Euro)) {
+            double imp = Double.parseDouble(Euro.getText().toString());
+            double calcolo = imp / tasso;
+            //setto la view
+            resDollari.setText(String.format("%.2f", calcolo) + "$");
+        }else{
+            Toast.makeText(this, "Importo inserito nullo", Toast.LENGTH_SHORT).show();
+        }
+        Euro.setText("");
+    }
+
+    private boolean isEmpty(EditText euro) {
+        String debug = Euro.getText().toString().trim();
+        return debug.length() == 0;
     }
 }
