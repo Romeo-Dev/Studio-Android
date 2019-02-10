d9ec014ce9ff6948448681945de1871894282b71
Romeo <romeodev1996@gmail.com>
2019-02-10 17:36:04 +0100

    inizializzazione del mio progetto Converti valute, create interfaccia grafica ed icona launcher

---
+++ ConvertitorediVaute/app/src/main/java/com/masterandroid/romeo/convertitoredivaute/MainActivity.java
@@ -0,0 +1,15 @@
+package com.masterandroid.romeo.convertitoredivaute;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.masterandroid.romeo.convertitoredivaute.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
